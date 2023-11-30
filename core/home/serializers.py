5461dc55fa0e3d73deb0ad9741822eb0708d0b33
dwanGim <nugoory20@gmail.com>
2023-12-01 04:08:00 +0900

    crud 테스트 완료

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -5,8 +5,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        # fields = ['name']
-        # exclude = ['name']
         fields = '__all__'
 
+    def validate(self, data):
+
+        special_characters ="!@#$%^&*()-+?_=,<>/"
+
+        if any(c in special_characters for c in data['name']):
+            raise serializers.ValidationError('이름에 특수문자는 허용되지 않습니다.')
+
+        if data['age']<18:
+            raise serializers.ValidationError('age는 18세 이상이어야합니다.')
+
+        return data
 
